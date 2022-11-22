6100aa7d8dc00980977109bffc4c38358bf19ef4
Enzo Prina <enzoprina9@gmail.com>
2022-11-21 23:57:25 -0300

    terminado

--- js/index.js
+++ js/index.js
@@ -34,3 +34,16 @@ window.addEventListener("resize", function(){
     }
 
 });
+
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#trucazo')
+
+$form.addEventListener('submit', handleSubmit)
+
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    $buttonMailto.setAttribute('href', `mailto:enzoprina9@gmail.com?subject=nombre ${form.get('name')} correo ${form.get('email')} body ${form.get('message') }`)
+    $buttonMailto.click()
+}
